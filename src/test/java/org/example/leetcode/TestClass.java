9848a6f67d26df77e53edfcc36942081801c8fd6
EgorkinMikhail <egorkin.mikhail@gmail.com>
2025-05-31 19:03:28 +0200

    magic square template

--- src/test/java/org/example/leetcode/TestClass.java
+++ src/test/java/org/example/leetcode/TestClass.java
@@ -3,6 +3,8 @@ package org.example.leetcode;
 import org.example.AlgorithmTasks.EasyAlgorithms.leetcode.NumArray;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class TestClass {
 
   @Test
@@ -12,5 +14,14 @@ public class TestClass {
     System.out.println(numArray.sumRange(0, 2));
     System.out.println(numArray.sumRange(2, 5));
     System.out.println(numArray.sumRange(0, 5));
+  }
+
+  @Test
+  public void name() {
+
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+    String listString = Arrays.toString(numbers).replaceAll("\\[", "").replaceAll("]", "");
+    System.out.println(listString.replaceAll("\\[", "").replaceAll("]", ""));
   }
 }
